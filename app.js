965ea1ea5de987c4ab35b23e3c3f449d638b4ba3
cijd-8133 <147498126+cijd-8133@users.noreply.github.com>
2023-10-10 15:27:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var parent =[React.createElement("div",{},React.createElement("h1",{},"welcome to react class")),React.createElement("p",{},
+"React lets you build user interfaces out of individual pieces called components"),React.createElement("h2",{},"react first assignment")]
+var htmlRoot=document.getElementById("root")
+var rootReact=ReactDOM.createRoot(htmlRoot)
+rootReact.render(parent)
