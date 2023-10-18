03b69b1e25a27876113c00216d076709d377dd39
cijd-8133 <147498126+cijd-8133@users.noreply.github.com>
2023-10-18 12:25:53 +0530

    react

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react"
+import ReactDOM from "react"
 var parent =[React.createElement("h3",{},React.createElement("h1",{},"welcome to react class")),React.createElement("p",{},
 "React lets you build user interfaces out of individual pieces called components"),React.createElement("h2",{},"react first assignment")]
 var htmlRoot=document.getElementById("root")
